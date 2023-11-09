0897107a86bfed9cd53e1a6d046a2be316e8c8d6
米国梁 <miguoliang@singhand.com>
2023-11-09 15:34:45 +0800

    update: use d3-force instead of elkjs

--- src/components/ui/Graph/GraphComponent.tsx
+++ src/components/ui/Graph/GraphComponent.tsx
@@ -10,12 +10,12 @@ import {
   useReactFlow,
 } from "reactflow";
 import { Edge as EdgeModel, Vertex as VertexModel } from "models";
-import ELK, { ElkExtendedEdge, ElkNode } from "elkjs/lib/elk.bundled";
-import { LayoutOptions } from "elkjs/lib/elk-api";
 import { GraphContext } from "./context";
 import FloatingEdge from "./FloatingEdge";
 import FloatingConnectionLine from "./FloatingConnectionLine";
 import "./GraphComponent.css";
+import * as d3 from "d3";
+import { SimulationLinkDatum, SimulationNodeDatum } from "d3-force";
 
 type NodeDataType = {
   label: string;
@@ -25,41 +25,32 @@ const edgeTypes = {
   floating: FloatingEdge,
 };
 
-const elk = new ELK();
-
-const defaultOptions: LayoutOptions = {
-  "elk.algorithm": "layered",
-  "elk.layered.spacing.nodeNodeBetweenLayers": "50",
-  "elk.spacing.nodeNode": "80",
-};
-
 export const GraphComponent = () => {
   const { vertices, edges } = useContext(GraphContext);
   const { setNodes, setEdges } = useReactFlow();
 
   useEffect(() => {
-    const reactFlowNodes = vertices2nodes(vertices);
-    const reactFlowEdges = edges2links(edges);
-    setEdges(reactFlowEdges);
-    const elkNodes = reactFlowNodesToElkNodes(reactFlowNodes);
-    const elkEdges = reactFlowEdgesToElkEdges(reactFlowEdges);
-    elk
-      .layout({
-        id: "root",
-        layoutOptions: defaultOptions,
-        children: elkNodes,
-        edges: elkEdges,
-      })
-      .then((g) => {
-        const nodes =
-          g.children?.map((c) => ({
-            id: c.id,
-            position: { x: c.x, y: c.y },
-            data: { label: c.labels?.[0].text },
-          })) || [];
-        setNodes(nodes as Node<NodeDataType>[]);
-      })
-      .catch(console.error);
+    const nodes = vertices as SimulationNodeDatum[];
+    const links = edges.map((e) => ({
+      source: e.inVertexId,
+      target: e.outVertexId,
+      id: `${e.inVertexId}-${e.name}-${e.outVertexId}`,
+    })) as SimulationLinkDatum<SimulationNodeDatum>[];
+    d3.forceSimulation(nodes)
+      .force(
+        "link",
+        d3
+          .forceLink()
+          .id((d) => (d as VertexModel).id)
+          .links(links)
+          .distance(500)
+          .strength(1),
+      )
+      .force("center", d3.forceCenter())
+      .on("end", () => {
+        setNodes(vertices2nodes(vertices));
+        setEdges(edges2links(edges));
+      });
   }, [vertices, edges]);
 
   return (
@@ -69,6 +60,7 @@ export const GraphComponent = () => {
       edgeTypes={edgeTypes}
       connectionLineComponent={FloatingConnectionLine}
       className="graph-component"
+      fitView
     >
       <Background />
       <Controls />
@@ -80,7 +72,10 @@ export const GraphComponent = () => {
 function vertices2nodes(vertices: VertexModel[]): Node<NodeDataType>[] {
   return vertices.map((v) => ({
     id: v.id,
-    position: { x: 0, y: 0 },
+    position: {
+      x: (v as SimulationNodeDatum).x || 0,
+      y: (v as SimulationNodeDatum).y || 0,
+    },
     data: { label: v.name },
     targetPosition: Position.Right,
     sourcePosition: Position.Left,
@@ -93,26 +88,5 @@ function edges2links(edges: EdgeModel[]): Edge[] {
     target: e.outVertexId,
     id: `${e.inVertexId}-${e.name}-${e.outVertexId}`,
     type: "floating",
-  }));
-}
-
-function reactFlowNodesToElkNodes(nodes: Node<NodeDataType>[]): ElkNode[] {
-  return nodes.map((node) => ({
-    id: node.id,
-    // width of the node in px.
-    width: 150,
-    // height of the node in px.
-    height: 75,
-    labels: [{ text: node.data.label }],
-  }));
-}
-
-function reactFlowEdgesToElkEdges(edges: Edge[]): ElkExtendedEdge[] {
-  return edges.map((edge) => ({
-    id: edge.id,
-    // the id of the source node of this edge.
-    sources: [edge.source],
-    // the id of the target node of this edge.
-    targets: [edge.target],
   }));
 }
